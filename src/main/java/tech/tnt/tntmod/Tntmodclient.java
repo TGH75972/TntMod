287f0cf516515e3dbed800e514951d3212eb5ff8
Harshit <darknode369@gmail.com>
2026-01-24 15:14:07 +0530

    Commit 2

---
+++ src/main/java/tech/tnt/tntmod/Tntmodclient.java
@@ -0,0 +1,8 @@
+package tech.tnt.tntmod;
+import net.fabricmc.api.ClientModInitializer;
+public class Tntmodclient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        
+    }
+}
