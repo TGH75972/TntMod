63af1a024e4d47e01b7fe770ccd23fe73e8259ab
Harshit <darknode369@gmail.com>
2026-01-24 14:57:38 +0530

    Commit 1

---
+++ src/main/java/tech/tnt/tntmod/TntmodDataGenerator.java
@@ -0,0 +1,11 @@
+package tech.tnt.tntmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TntmodDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
