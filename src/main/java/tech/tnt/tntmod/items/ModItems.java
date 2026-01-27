344f3486eeb5824ef5d84434fc573051c834bbf4
Harshit <darknode369@gmail.com>
2026-01-27 13:53:11 +0530

    commit

--- src/main/java/tech/tnt/tntmod/items/ModItems.java
+++ src/main/java/tech/tnt/tntmod/items/ModItems.java
@@ -5,10 +5,10 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.util.Identifier;
-import tech.tnt.tntmod.Tntmod;
+import tech.tnt.tntmod.Tntmod;           
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 
-public class ModItems {
+public class ModItems { 
 
 public static final Item TNT_LAUNCHER= registerItem("tnt_launcher",new Item(new Item.Settings().maxCount(1)));
     private static Item registerItem(String name, Item item) {
